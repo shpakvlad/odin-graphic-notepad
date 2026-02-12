d997151def7307d54cbd7e6eda8275e270ddd943
ainol09061976 <sendsay>
2026-02-12 10:49:42 +0200

    Added base functionality
    Now I can draw in net (need optimisation!!!)

--- script/script.js
+++ script/script.js
@@ -1,12 +1,27 @@
 
 let graphicPlate = document.querySelector(".graphic");
+graphicPlate.addEventListener('dragstart', (e) => {
+    e.preventDefault();
+});
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 8100; i++) {
     let newDiv = document.createElement("div");
 
     newDiv.classList.add("one-pixel");
-    newDiv.style.width = (900 / 10) + "px";
-    newDiv.style.height = (900 / 10) + "px";
+    newDiv.style.width = (900 / 90) + "px";
+    newDiv.style.height = (900 / 90) + "px";
+
+     newDiv.addEventListener('mouseenter', (e) => {
+         if (e.buttons === 1) {
+             e.target.style.backgroundColor = 'red';
+         }
+     })
+
+    newDiv.addEventListener('mousedown', (e) => {
+
+            e.target.style.backgroundColor = 'red';
+
+    });
 
     graphicPlate.insertAdjacentElement("afterbegin", newDiv);
 }
